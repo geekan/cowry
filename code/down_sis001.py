8801240e9a8ad4af584d56ec8a66dba03ca8f8f1
GeekAn <geekan@foxmail.com>
2013-10-01 18:48:20 +0800

    update

--- code/down_sis001.py
+++ code/down_sis001.py
@@ -31,6 +31,11 @@ http_proxy = "http://localhost:8086"
 use_proxy = True
 http_proxys = {'http':http_proxy}
 wtfdir = 'test'
+mode = 'see' # see, down
+analytics_file = 'test.txt'
+forum_ids = {'YM' : 230, 'WM' : 143}
+
+text_order = ['star','url','title','comment','view','time']
 
 def get_valid_filename(filename):
     keepcharacters = (' ','.','_')
@@ -91,12 +96,12 @@ def down_imgs_from_url(url):
     torrents = re.findall('a href=\"(?P<url>attach[=0-9a-zA-Z\.\?]+).*?>(?P<title>[^<>\"]*?torrent)', content, re.M | re.S)
     
     imgs = set(imgs)
-    st = set(torrents)
-    print(imgs, st)
+    torrent_set = set(torrents)
+    print(imgs, torrent_set)
     for img in imgs:
         down_link(img, dirname + '/' + get_valid_filename(os.path.basename(img)))
-    for t in st:
-        down_link(base_url + t[0], dirname + '/' + get_valid_filename(t[1]))
+    for torrent in torrent_set:
+        down_link(base_url + torrent[0], dirname + '/' + get_valid_filename(torrent[1]))
 
     return
 
@@ -147,13 +152,15 @@ def get_links_from_page(url):
         for h in topics_html:
             gd = re.search(p, h, re.M | re.S).groupdict()
             gd['url'] = base_url + gd['url']
-            topics[gd['title']] = gd
-            print(topics[gd['title']])
+            topic_index = gd['title']
+            topics[topic_index] = gd
+            print(topics[topic_index])
     except Exception as e:
         print(e)
-
-    for i in topics:
-        down_link_imgs_torrents(topics[i])
+    
+    if mode is 'down':
+        for i in topics:
+            down_link_imgs_torrents(topics[i])
 
     return topics
 
@@ -210,8 +217,56 @@ def down_imgs_torrents():
         for page in pages:
             t[forum_id,page].join()
 
+def write_topic_list_to_file(topic_list, filename = 'test.txt'):
+    f = open(filename, 'wb')
+    #binary = str(topic_list).encode('utf8')
+    for topic in topic_list: # topic is a dict
+        for key in text_order:
+            value = topic[key]
+            binary = (str(value) + ',').encode('utf8')
+            f.write(binary)
+        f.write('\n'.encode('utf8'))
+    
+
+def down_topics_and_store(forum):
+    install_proxy()
+    base_forum_url = base_url + 'forum-{0}-{1}.html'
+    #base_forum_url.format(forum_id, page)
+    topics = dict();
+    
+    for page in range(1,1024):
+        topics.update(get_links_from_page(base_forum_url.format(forum, page)))
+
+    topic_list = list()
+    #topic_list = sorted(list(topics), key=lambda topic: topic['star'])
+    for value in topics.values():
+        temp = value
+        topic_list.append(temp)
+    topic_list.sort(key=lambda x: int(x['star']), reverse = True)
+    write_topic_list_to_file(topic_list, filename = str(forum) + analytics_file)
+
+    return 
+
+class ThreadDownTopicsAnalytics(threading.Thread):
+    forum_id = None
+    def __init__(self, forum_id):
+        threading.Thread.__init__(self)
+        self.forum_id = forum_id
+    
+    def run(self):
+        down_topics_and_store(self.forum_id)
+
+def get_all_analytics():
+    f1 = ThreadDownTopicsAnalytics(forum_ids['YM'])
+    f2 = ThreadDownTopicsAnalytics(forum_ids['WM'])
+    f1.start()
+    f2.start()
+    
+
 def main():
-    down_imgs_torrents()
+    #down_imgs_torrents()
+    # 下载所有topics，排序，输入文件
+    get_all_analytics()
 
 start = time.time()
 if __name__ == '__main__':
