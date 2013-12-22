8a61afc2e197bdf21d0f6a9209caa2d1933a4e71
GeekAn <geekan@foxmail.com>
2013-12-22 14:51:00 +0800

    add git stat counter

---
+++ code/git_stat_counter.sh
@@ -0,0 +1,2 @@
+git log --author="$1" --pretty=tformat: --numstat \
+| gawk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf "added lines: %s removed lines : %s total lines: %s\n",add,subs,loc }' -
