26b09e0688b26bc150ae8a7a2c32006597072e10
GeekAn <geekan@foxmail.com>
2014-02-10 22:16:23 +0800

    leetcode: update permutations2

--- code/leetcode/permutations2.py
+++ code/leetcode/permutations2.py
@@ -1,25 +1,31 @@
-def all_perms(elements):
-    if len(elements) <=1:
-        yield elements
-    else:
-        for perm in all_perms(elements[1:]):
-            for i in range(len(elements)):
-                yield perm[:i] + elements[0:1] + perm[i:]
+class unique_element:
+    def __init__(self,value,occurrences):
+        self.value = value
+        self.occurrences = occurrences
 
-def unique(iter):
-    s = set()
-    for i in iter:
-        if i in s:
-            continue
-        else:
-            s.add(i)
-    return list(s)
+def perm_unique(elements):
+    eset=set(elements)
+    listunique = [unique_element(i,elements.count(i)) for i in eset]
+    u=len(elements)
+    return perm_unique_helper(listunique,[0]*u,u-1)
+
+def perm_unique_helper(listunique,result_list,d):
+    if d < 0:
+        yield tuple(result_list)
+    else:
+        for i in listunique:
+            if i.occurrences > 0:
+                result_list[d]=i.value
+                i.occurrences-=1
+                for g in  perm_unique_helper(listunique,result_list,d-1):
+                    yield g
+                i.occurrences+=1
 
 class Solution:
     # @param num, a list of integer
     # @return a list of lists of integers
     def permuteUnique(self, num):
-        return unique(all_perms(num))
+        return list(perm_unique(num))
 
 s = Solution()
-print s.permute([1,2,3,4])
+print s.permuteUnique([1,2,3,3,4])
