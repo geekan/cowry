ba49c3ae0ffcd8b648e416ce6dfdd459f73994d8
GeekAn <geekan@foxmail.com>
2014-03-13 23:31:20 +0800

    test:update

--- code/c/test/test_sizeof.c
+++ code/c/test/test_sizeof.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int func(int x)
+{
+
+    int arr[x];
+    printf("func: %d %d\n", sizeof(arr), sizeof(arr[x++]));
+    printf("%d\n", x);
+    return 0;
+}
+
 int main(void)
 {
     int x = 0;
@@ -7,4 +16,6 @@ int main(void)
     printf("x:%d a:%d b:%d\n", sizeof(x++), sizeof(a), sizeof(b));
     printf("x:%d a:%d b:%d\n", sizeof(x + 1), sizeof(a + 1), sizeof(b + 1));
     printf("x:%d a:%d b:%d\n", x, sizeof(a), sizeof(b));
+    printf("x:%d a:%d b:%d\n", x, sizeof(a), sizeof(b));
+    func(10);
 }
