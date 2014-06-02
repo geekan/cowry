b3ac9dd277e750119b0dbf7f01c27d3ca62b9435
geekan <geekan@foxmail.com>
2014-06-02 13:31:02 +0800

    rename rules dir and update config

--- etc/shortcuts/config.sh
+++ etc/shortcuts/config.sh
@@ -5,7 +5,7 @@
 #######
 # MAC #
 #######
-cat rules/* > ~/.bashrc.alexanderwu
+cat alias.rules/* > ~/.bashrc.alexanderwu
 grep bashrc ~/.bash_profile || echo '[ -r ~/.bashrc ] && source ~/.bashrc' >> ~/.bash_profile
 grep alexanderwu ~/.bashrc || echo "source ~/.bashrc.alexanderwu" >> ~/.bashrc
 source ~/.bashrc
