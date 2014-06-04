72fed726c6175435ffee1fbd22ba57625461a504
geekan <geekan@foxmail.com>
2014-06-04 14:55:26 +0800

    update config.sh

--- etc/shortcuts/config.sh
+++ etc/shortcuts/config.sh
@@ -1,4 +1,23 @@
 # coding: utf-8
 
-curl -L http://install.ohmyz.sh | sh
-cat alias.rules/*.rules > ~/.oh-my-zsh/custom/alexanderwu.zsh
+config_mac()
+{
+    curl -L http://install.ohmyz.sh | sh
+    cat alias.rules/*.rules > ~/.oh-my-zsh/custom/alexanderwu.zsh
+}
+
+config_linux()
+{
+    echo 'not done yet..'
+}
+
+config_windows()
+{
+    echo 'not done yet..'
+}
+
+[ "$(uname)" == "Darwin" ] && config_mac()
+[ "$(expr substr $(uname -s 1 5))" == "Linux" ] && config_linux()
+[ "$(expr substr $(uname -s 1 10))" == "MINGW32_NT" ] && config_windows()
+[ "$(expr substr $(uname -s 1 6))" == "CYGWIN" ] && config_windows()
+
