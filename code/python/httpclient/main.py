4d502455c9d8b68304f0a623f69532c07d19b3c4
geekan <geekan@foxmail.com>
2014-08-02 08:13:14 +0800

    http client: use python asyncore.

---
+++ code/python/httpclient/main.py
@@ -0,0 +1,29 @@
+import asyncore, socket
+
+class HTTPClient(asyncore.dispatcher):
+
+    def __init__(self, host, path):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect( (host, 80) )
+        self.buffer = 'GET %s HTTP/1.0\r\n\r\n' % path
+
+    def handle_connect(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+
+    def handle_read(self):
+        print self.recv(8192)
+
+    def writable(self):
+        return (len(self.buffer) > 0)
+
+    def handle_write(self):
+        sent = self.send(self.buffer)
+        self.buffer = self.buffer[sent:]
+
+
+client = HTTPClient('www.python.org', '/')
+asyncore.loop()
