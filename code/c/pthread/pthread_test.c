895b8848895ea20f546d2905226fee3bf96a9bc0
geekan <geekan@foxmail.com>
2014-08-18 18:43:41 +0800

    pthread_test: update

--- code/c/pthread/pthread_test.c
+++ code/c/pthread/pthread_test.c
@@ -3,42 +3,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define PRINT(STH) printf("%s\n", #STH)
+#define STR(STH) #STH
+#define DO(STH) \
+    printf("%s:%d\n", STR(STH), STH);
 
 int count = 0;
-#define SIG_MAX 30
+#define SIGNAL_MAX 30
 
-struct prodcons {
+struct product_condition {
     pthread_mutex_t lock;
-    pthread_cond_t sig[SIG_MAX];
-    int ref;
+    pthread_cond_t sig[SIGNAL_MAX];
+    int ready;
 };
 
-struct prodcons buffer;
-int init(struct prodcons *b)
+struct product_condition buffer;
+int init(struct product_condition *b)
 {
     int i = 0;
     pthread_mutex_init(&b->lock, NULL);
-    for (i = 0; i < SIG_MAX; i++) {
+    for (i = 0; i < SIGNAL_MAX; i++) {
         pthread_cond_init(&b->sig[i], NULL);
     }
-    b->ref = 0;
+    b->ready = 0;
+    return 0;
 }
 
-void put(struct prodcons *b, int data, int sig)
+void put(struct product_condition *b, int data, int sig)
 {
     while(1) {
         pthread_mutex_lock(&b->lock);
-        b->ref |= (1 << sig);
+        // Say I'm ready to receive a signal..
+        // But not a stable way.
+        b->ready |= (1 << sig);
+        // Unlock the mutex and wait for the signal.
+        // If signaled, reacquire the mutex.
         pthread_cond_wait(&b->sig[sig], &b->lock);
-        b->ref &= ~(1 << sig);
+
+        // b->ready &= ~(1 << sig);
         count++;
         printf("signal: %d data: %d count:%d\n", sig, data, count);
+
         pthread_mutex_unlock(&b->lock);
 
-        int tmp;
-        while( !(tmp = (b->ref & (1 << ((sig + 1) % SIG_MAX)))) )
-            printf("while-tmp:%d\n", tmp);
-        pthread_cond_signal(&b->sig[(sig + 1) % SIG_MAX]);
+        // Tell next pthread to continue put.
+        pthread_cond_signal(&b->sig[(sig + 1) % SIGNAL_MAX]);
     }
 }
 
@@ -59,24 +68,29 @@ void *producer(void *data)
 int main(void)
 {
     int i;
-    int resource[SIG_MAX];
-    pthread_t p[SIG_MAX];
+    int resource[SIGNAL_MAX];
+    pthread_t p[SIGNAL_MAX];
     void *retval;
     init(&buffer);
 
-    for (i = 0; i < SIG_MAX; i++) {
+    printf("Starting %d pthreads...\n"
+           "Pthreads would print numbers one by one~\n\n", SIGNAL_MAX);
+
+    sleep(1);
+    for (i = 0; i < SIGNAL_MAX; i++) {
         resource[i] = i;
         pthread_create(&p[i], NULL, producer, &resource[i]);
     }
 
-    printf("Starting %d pthreads...\n"
-           "Pthreads would print numbers one by one~\n\n", SIG_MAX);
-    while(buffer.ref != ((1 << SIG_MAX) - 1));
-    printf("\nSending signal[0] to trigger a start\n");
-    sleep(1);
-    pthread_cond_signal(&buffer.sig[0]);
+    // Make sure all pthreads are ready.
+    while(buffer.ready != ((1 << SIGNAL_MAX) - 1));
 
-    for (i = 0; i < SIG_MAX; i++) {
+    printf("\nPress enter to send signal[0] to trigger a start\n");
+    getchar();
+
+    DO(pthread_cond_signal(&buffer.sig[0]));
+
+    for (i = 0; i < SIGNAL_MAX; i++) {
         pthread_join(p[i], &retval);
     }
     return 0;
