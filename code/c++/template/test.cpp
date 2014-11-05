3a75dcde95f1e192de580c4e531bfa6c7c0fe1ad
geekan <geekan@foxmail.com>
2014-11-05 10:54:33 +0800

    c++ template: update

--- code/c++/template/test.cpp
+++ code/c++/template/test.cpp
@@ -5,10 +5,26 @@ using namespace std;
 
 namespace test {
 
+template <class T1, class T2>
+class testClass {
+private:
+    T1 I;
+    T2 J;
+public:
+    testClass(T1 a, T2 b);
+    void show();
+};
+
 template <class T> 
 T max(T a, T b)
 {
     return (a > b) ? a : b;
+}
+
+template <class T1, class T2>
+void testClass::show()
+{
+    cout << I << "|" << J << endl;
 }
 
 }
