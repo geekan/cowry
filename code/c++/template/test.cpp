670b7112d60f01861f800df514d545164a54f061
geekan <geekan@foxmail.com>
2014-11-05 10:44:46 +0800

    c++ template: added

--- code/c++/template/test.cpp
+++ code/c++/template/test.cpp
@@ -3,7 +3,20 @@
 
 using namespace std;
 
+namespace test {
+
+template <class T> 
+T max(T a, T b)
+{
+    return (a > b) ? a : b;
+}
+
+}
+
 int main(void)
 {
+    int a = 1, b = 5;
+    cout << test::max(a, b) << endl;
     return 0;
 }
+
