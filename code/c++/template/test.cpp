dba25e61184c51479ee3fc79a8ceed80862b48d8
geekan <geekan@foxmail.com>
2014-11-05 11:00:35 +0800

    c++ template: update

--- code/c++/template/test.cpp
+++ code/c++/template/test.cpp
@@ -22,12 +22,15 @@ T max(T a, T b)
 }
 
 template <class T1, class T2>
-void testClass::show()
+testClass<T1, T2>::testClass(T1 a, T2 b): I(a), J(b) {}
+
+template <class T1, class T2>
+void testClass<T1, T2>::show()
 {
     cout << I << "|" << J << endl;
 }
 
-}
+} // end for test namespace
 
 int main(void)
 {
@@ -35,6 +38,12 @@ int main(void)
     double x = 3, y = 8;
     cout << test::max(a, b) << endl;
     cout << test::max(x, y) << endl;
+
+    test::testClass<int, int> class_ii(10, 12);
+    class_ii.show();
+    test::testClass<int, double> class_id(13, 5.5);
+    class_id.show();
+
     return 0;
 }
 
