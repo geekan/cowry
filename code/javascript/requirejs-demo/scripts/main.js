9def16395338ba8cbb1362bb8e2bd4d12da53cce
geekan <geekan@foxmail.com>
2015-08-21 02:18:44 +0800

    require jquery

--- code/javascript/requirejs-demo/scripts/main.js
+++ code/javascript/requirejs-demo/scripts/main.js
@@ -1,7 +1,24 @@
+//
+requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'jquery-1.11.3'
+    }
+});
+
 // main.js
 require(['wtf'], function (math){
     var x = math.add(1,1);
-    alert(x);
+    // alert(x);
     console.log(x);
+    console.log($);
 });
 
