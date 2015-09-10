afe4797eca36964dd825bba6f5ffdc6c7552bc5b
geekan <geekan@foxmail.com>
2015-09-10 12:01:21 +0000

    done neural-style

--- install/neural-style.sh
+++ install/neural-style.sh
@@ -11,3 +11,7 @@ luarocks install loadcaffe
 cd /git/
 git clone https://github.com/jcjohnson/neural-style.git
 cd neural-style
+
+sh models/download_models.sh
+th neural_style.lua -gpu -1 -print_iter -1
+
