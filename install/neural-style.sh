760d8a5416a2f72d11dc5e60221473151dac08c2
geekan <geekan@foxmail.com>
2015-09-10 07:27:24 +0000

    update to step2

--- install/neural-style.sh
+++ install/neural-style.sh
@@ -2,3 +2,8 @@ cd /git/
 curl -s https://raw.githubusercontent.com/torch/ezinstall/master/install-deps | bash
 git clone https://github.com/torch/distro.git /git/torch --recursive
 cd /git/torch; ./install.sh
+
+source ~/.zshrc
+
+sudo apt-get install libprotobuf-dev protobuf-compiler
+luarocks install loadcaffe
