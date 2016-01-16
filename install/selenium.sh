3ab0e4f6508758902a0f01b7a2a938fa7d854d45
geekan <geekan@foxmail.com>
2016-01-16 15:20:48 +0000

    add selenium install script.

---
+++ install/selenium.sh
@@ -0,0 +1,5 @@
+pip install -U selenium
+wget http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.0.jar
+#java -jar selenium-server-standalone-2.49.0.jar
+wget http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
